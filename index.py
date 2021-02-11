5fdc1d0508880c95ef0d8b33c7f8345755f68bb8
AKosmachyov <a.kosmachyov@gmail.com>
2021-02-11 18:48:39 +0300

    implement failed field stat

--- index.py
+++ index.py
@@ -9,8 +9,11 @@ class TestCase:
         result = TestResult()
         result.testStarted()
         self.setUp()
-        method = getattr(self, self.name)
-        method()
+        try:
+            method = getattr(self, self.name)
+            method()
+        except:
+            result.testFailed()
         self.tearDown()
         return result
 
@@ -38,12 +41,16 @@ class WasRun(TestCase):
 class TestResult:
     def __init__(self):
         self.runCount = 0
+        self.errorCount = 0
 
     def testStarted(self):
         self.runCount += 1
 
+    def testFailed(self):
+        self.errorCount += 1
+
     def summary(self):
-        return "{} run, 0 failed".format(self.runCount)
+        return "{} run, {} failed".format(self.runCount, self.errorCount)
 
 
 class TestCaseTest(TestCase):
@@ -60,9 +67,16 @@ class TestCaseTest(TestCase):
     def testFailedResult(self):
         test = WasRun("testBrokenMethod")
         result = test.run()
-        assert ("1 run, 1 failed", result.summary())
+        assert ("1 run, 1 failed" == result.summary())
+
+    def testFailedResultFormatting(self):
+        result = TestResult()
+        result.testStarted()
+        result.testFailed()
+        assert ("1 run, 1 failed" == result.summary())
 
 
 TestCaseTest("testTemplateMethod").run()
 TestCaseTest("testResult").run()
-# TestCaseTest("testFailedResult").run()
+TestCaseTest("testFailedResult").run()
+TestCaseTest("testFailedResultFormatting").run()
